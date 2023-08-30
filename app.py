93e2cbe4f4e0ebe4abf9479a92c6b86d16545897
sasidn <sasidharannissy@gmail.com>
2023-08-30 19:49:46 +0200

    updated withmysqldb

--- app.py
+++ app.py
@@ -1,4 +1,21 @@
 from flask import Flask, render_template, request, redirect
+from dotenv import load_dotenv
+load_dotenv()
+import os
+import MySQLdb
+
+connection = MySQLdb.connect(
+  host= os.getenv("DB_HOST"),
+  user=os.getenv("DB_USERNAME"),
+  passwd= os.getenv("DB_PASSWORD"),
+  db= os.getenv("DB_NAME"),
+  autocommit = True,
+  ssl_mode = "VERIFY_IDENTITY",
+  ssl      = {
+    "ca": "/etc/ssl/cert.pem"
+  }
+)
+
 
 app = Flask(__name__)
 @app.route('/')
