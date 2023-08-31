c134975f40ef84831e41da3c09580057ace25dc9
sasidn <sasidharannissy@gmail.com>
2023-08-31 08:24:28 +0200

    deploying with mySQL

--- app.py
+++ app.py
@@ -1,12 +1,29 @@
 from flask import Flask, render_template, request, redirect
 
-
+from dotenv import load_dotenv
+load_dotenv()
+import os
+import MySQLdb
+connection = MySQLdb.connect(
+  host= os.getenv("DB_HOST"),
+  user=os.getenv("DB_USERNAME"),
+  passwd= os.getenv("DB_PASSWORD"),
+  db= os.getenv("DB_NAME"),
+  autocommit = True,
+  ssl_mode = "VERIFY_IDENTITY",
+  ssl      = {
+    "ca": "/etc/ssl/cert.pem"
+  }
+)
 
 
 app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('questionaire.html')
+
+def cbt():
+    return render_template('cbt.html')
 
 
 
